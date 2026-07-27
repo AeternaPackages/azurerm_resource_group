b6c1e986c329f7e43e95898eb5fe08dda58fac00
Daniel Allen <dan@aeternagroup.net>
2026-07-27 18:30:47 +0100

    Update to schema version v4.81.0

--- main.tf
+++ main.tf
@@ -95,66 +95,66 @@ locals {
 }
 
 module "resource_groups" {
-  source          = "git::https://github.com/AeternaModules/azurerm_resource_group.git?ref=v4.80.0"
+  source          = "git::https://github.com/AeternaModules/azurerm_resource_group.git?ref=v4.81.0"
   resource_groups = local.resource_groups
 }
 
 module "resource_deployment_script_azure_clis" {
-  source                                = "git::https://github.com/AeternaModules/azurerm_resource_deployment_script_azure_cli.git?ref=v4.80.0"
+  source                                = "git::https://github.com/AeternaModules/azurerm_resource_deployment_script_azure_cli.git?ref=v4.81.0"
   resource_deployment_script_azure_clis = local.resource_deployment_script_azure_clis
   depends_on                            = [module.resource_groups]
 }
 
 module "resource_deployment_script_azure_power_shells" {
-  source                                        = "git::https://github.com/AeternaModules/azurerm_resource_deployment_script_azure_power_shell.git?ref=v4.80.0"
+  source                                        = "git::https://github.com/AeternaModules/azurerm_resource_deployment_script_azure_power_shell.git?ref=v4.81.0"
   resource_deployment_script_azure_power_shells = local.resource_deployment_script_azure_power_shells
   depends_on                                    = [module.resource_groups]
 }
 
 module "resource_group_cost_management_exports" {
-  source                                 = "git::https://github.com/AeternaModules/azurerm_resource_group_cost_management_export.git?ref=v4.80.0"
+  source                                 = "git::https://github.com/AeternaModules/azurerm_resource_group_cost_management_export.git?ref=v4.81.0"
   resource_group_cost_management_exports = local.resource_group_cost_management_exports
   depends_on                             = [module.resource_groups]
 }
 
 module "resource_group_cost_management_views" {
-  source                               = "git::https://github.com/AeternaModules/azurerm_resource_group_cost_management_view.git?ref=v4.80.0"
+  source                               = "git::https://github.com/AeternaModules/azurerm_resource_group_cost_management_view.git?ref=v4.81.0"
   resource_group_cost_management_views = local.resource_group_cost_management_views
   depends_on                           = [module.resource_groups]
 }
 
 module "resource_group_policy_assignments" {
-  source                            = "git::https://github.com/AeternaModules/azurerm_resource_group_policy_assignment.git?ref=v4.80.0"
+  source                            = "git::https://github.com/AeternaModules/azurerm_resource_group_policy_assignment.git?ref=v4.81.0"
   resource_group_policy_assignments = local.resource_group_policy_assignments
   depends_on                        = [module.resource_groups]
 }
 
 module "resource_group_policy_exemptions" {
-  source                           = "git::https://github.com/AeternaModules/azurerm_resource_group_policy_exemption.git?ref=v4.80.0"
+  source                           = "git::https://github.com/AeternaModules/azurerm_resource_group_policy_exemption.git?ref=v4.81.0"
   resource_group_policy_exemptions = local.resource_group_policy_exemptions
   depends_on                       = [module.resource_groups]
 }
 
 module "resource_group_policy_remediations" {
-  source                             = "git::https://github.com/AeternaModules/azurerm_resource_group_policy_remediation.git?ref=v4.80.0"
+  source                             = "git::https://github.com/AeternaModules/azurerm_resource_group_policy_remediation.git?ref=v4.81.0"
   resource_group_policy_remediations = local.resource_group_policy_remediations
   depends_on                         = [module.resource_groups]
 }
 
 module "resource_group_template_deployments" {
-  source                              = "git::https://github.com/AeternaModules/azurerm_resource_group_template_deployment.git?ref=v4.80.0"
+  source                              = "git::https://github.com/AeternaModules/azurerm_resource_group_template_deployment.git?ref=v4.81.0"
   resource_group_template_deployments = local.resource_group_template_deployments
   depends_on                          = [module.resource_groups]
 }
 
 module "resource_management_private_links" {
-  source                            = "git::https://github.com/AeternaModules/azurerm_resource_management_private_link.git?ref=v4.80.0"
+  source                            = "git::https://github.com/AeternaModules/azurerm_resource_management_private_link.git?ref=v4.81.0"
   resource_management_private_links = local.resource_management_private_links
   depends_on                        = [module.resource_groups]
 }
 
 module "resource_management_private_link_associations" {
-  source                                        = "git::https://github.com/AeternaModules/azurerm_resource_management_private_link_association.git?ref=v4.80.0"
+  source                                        = "git::https://github.com/AeternaModules/azurerm_resource_management_private_link_association.git?ref=v4.81.0"
   resource_management_private_link_associations = local.resource_management_private_link_associations
   depends_on                                    = [module.resource_management_private_links]
 }
